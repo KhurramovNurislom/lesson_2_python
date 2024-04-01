ae81bf44343775ffdb5a1f26bb337d8093c9581f
KhurramovNurislom <AlisherUzakov2406@gmail.com>
2024-04-01 17:23:35 +0500

    homework3

--- lesson_2_python.py
+++ lesson_2_python.py
@@ -7,8 +7,11 @@ birthDayList = ["24.06.1996", "24/05/1991", "14-02-1997", "24 06 1996", "2406199
 phoneNumberList = ["94-648-83-34","+998977006551","97 619 97 97", "88 789 83 34", "97 777 44 55", "78 456 83 34"]
 addressList = ["Chilonzor birnima birnimalar", "Mirzo ulug'bek birnima bir nimalar", "Yunusobod birnima birnimalar", "chILONZOR bironnima bironnima", "Shayxontoxur tumani", "Olmazor tumani"]
 
+allLists = [nameList, surnameList, fatherNameList, birthDayList, phoneNumberList, addressList]
+
 
 def printFull(index):
+    k=0
     k=index+1
     print(f"""{k} - shaxs:""")
     k=0
@@ -30,12 +33,13 @@ def clearPunt(word):
 
 def ifForSearch(index, search, word):
     if clearPunt(search) in clearPunt(word):
-        printFull(index)  
-    
+        printFull(index)
+            
 while True:
     print("""\n     Hi! select the required command:""")
     print(""" 1 => data entry   2 => Search   3 => view all data   4 => Exit""")
     com = int (input("select the command =>>> "))
+    
     if(com == 1):
         print("\n<<<<<<<<<<<<<<<< Enter data >>>>>>>>>>>>>>>>>")
         nameList.append(input("Name: "))
@@ -44,22 +48,17 @@ while True:
         birthDayList.append(input("Birth day: "))
         phoneNumberList.append(input("Phone number: "))
         addressList.append(input("Address: "))
+    
     elif(com == 2):
-        k=0
-        search = input("kalit so'zni kiriting: ")
-        for index, word in enumerate(nameList):
-            ifForSearch(index, search, word)        
-        for index, word  in enumerate(surnameList):
-            ifForSearch(index, search, word)         
-        for index, word  in enumerate(fatherNameList):
-            ifForSearch(index, search, word)            
-        for index, word  in enumerate(birthDayList):
-            ifForSearch(index, search, word)            
-        for index, word  in enumerate(phoneNumberList):
-            ifForSearch(index, search, word)             
-        for index, word  in enumerate(addressList):
-            ifForSearch(index, search, word)             
-              
+        while True:
+            search = input("kalit so'zni kiriting: ")
+            for i in range(len(allLists)):
+                for index, word in enumerate(allLists[i]):
+                    ifForSearch(index, search, word) 
+        
+
+                  
+            
     elif(com == 3):
         for index in range(len(nameList)):
            printFull(index)
